4d0f35b0ee4424e518e1c48d17821722155b5bb8
Eugene Y. Sun <eug.sun@gmail.com>
2013-02-11 17:25:47 -0500

    added username and email config for git.

--- setup-github.sh
+++ setup-github.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Set up git user and email
+git config --global user.name "Eugene Y. Sun"
+git config --global user.email eug.sun@gmail.com
+
 # Generate SSH
 ssh-keygen -t rsa -C "eug.sun@gmail.com"
 
